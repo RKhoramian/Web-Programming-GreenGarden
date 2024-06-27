3d47281bcfea1082b7c28314788616e4ed132f05
YasminTorki <ytorkib@gmail.com>
2024-06-28 01:23:42 +0430

    add signup and login page first try

---
+++ WP-GG/frontend/SignUpPage/signup.js
@@ -0,0 +1,9 @@
+document.getElementById('submitsign').addEventListener('click', function() {
+    console.log("hsjgj");
+    window.location.href = '../HomePage/index.html'; // Replace 'index.html' with the path to your home page
+});
+
+document.getElementById('backButton').addEventListener('click', function() {
+    window.location.href = '../homepage/index.html'; // Replace 'index.html' with the path to your home page
+});
+
